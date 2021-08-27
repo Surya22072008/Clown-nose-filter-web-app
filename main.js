8e8d5d141c91acd9ebeb99de8b3122c7008bd930
Surya22072008 <78208952+Surya22072008@users.noreply.github.com>
2021-08-27 10:23:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+clown_nose = "https://i.postimg.cc/j56GN65k/clown-nose.png";
 }
 function setup(){
     canvas = createCanvas(300,300);
@@ -12,6 +14,7 @@ function setup(){
 }
 function draw(){
 image(video,0,0,300,300);
+image(clown_nose,noseX,noseY,30,30);
 }
 function take_snapshot(){
     save('myfilter.png');
@@ -22,7 +25,9 @@ function modelLoaded(){
 function getPoses(results) {
     if(results.length > 0 ){
         console.log(results);
-        console.log("nose x ="+ results[0].pose.nose.x);
-        console.log("nose y ="+ results[0].pose.nose.y);
+        noseX =  results[0].pose.nose.x ;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x ="+ noseX);
+        console.log("nose y ="+ noseY);
     }
 }
